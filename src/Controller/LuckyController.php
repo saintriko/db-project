0355e386f54eb591dafc40c42b41115b291aa4f6
Maks Semeev <makssemeev@gmail.com>
2020-04-22 19:08:57 +0300

    created Login controller

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -1,11 +1,16 @@
 <?php
-// src/Controller/LuckyController.php
 namespace App\Controller;
-
+use Exception;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class LuckyController
 {
+    /**
+     * @Route("/lucky/number", name="luckycontroller")
+     * @return Response
+     * @throws Exception
+     */
     public function number()
     {
         $number = random_int(0, 100);
