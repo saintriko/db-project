d4e9d509a07c1a006992ee651402e1d2e145a44b
Maks Semeev <makssemeev@gmail.com>
2020-05-07 16:51:24 +0300

    addded services in PlaceController

--- src/Controller/PlaceController.php
+++ src/Controller/PlaceController.php
@@ -4,16 +4,38 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\PlaceRepository;
+use App\Repository\WorkTimeRepository;
 
 class PlaceController extends AbstractController
 {
     /**
      * @Route("/place/{id}", name="place")
      */
-    public function index(int $id)
+    public function index(int $id, PlaceRepository $placeRepository, WorkTimeRepository $WorkTimeRepository)
     {
+        $place = $placeRepository->findOneBy(['id' => $id]);
+        $placeName = $place->getName();
+        $placeAddress = $place->getAddress();
+        $placeCategory = $place->getCategory()->getName();
+        $placePhoneNumber = $place->getPhoneNumber();
+        $placeDescription = $place->getDescription();
+
+        $placeServices = $place->getPlaceHasServices();
+        $services = [];
+        foreach ($placeServices as $service) {
+            array_push($services, [$service->getService()->getName(), $service->getPrice()]);
+        }
+
+        $placeWorkTimes = $place->getWorkTimes();
+
         return $this->render('place/index.html.twig', [
-            'controller_name' => "Place".$id,
+            'place_name' => $placeName,
+            'place_address' => $placeAddress,
+            'place_category' => $placeCategory,
+            'place_phone_number' => $placePhoneNumber,
+            'place_description' => $placeDescription,
+            'place_services' => $services
         ]);
     }
 }
