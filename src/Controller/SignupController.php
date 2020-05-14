40e9dcc66aa39be87187c44462f50b30b0b2ef3e
Maks Semeev <makssemeev@gmail.com>
2020-05-14 15:22:49 +0300

    changed role name ROLE_USER

--- src/Controller/SignupController.php
+++ src/Controller/SignupController.php
@@ -34,7 +34,7 @@ class SignupController extends AbstractController
             } else {
 
                     $role = $roleRepository
-                        ->findOneBy(['name' => 'user']);
+                        ->findOneBy(['name' => 'ROLE_USER']);
                     $user->setRole($role);
                     $user->setToken(sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535)));
 
