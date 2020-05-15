05f97166dd4b7db14bd3590cb2ef7b93d0d6d188
Riko <makssemeev@gmail.com>
2020-05-15 15:11:58 +0300

    file uploading

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,9 +7,12 @@ use App\Entity\WorkTime;
 use App\Repository\CategoryRepository;
 use App\Repository\PlaceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 class DefaultController extends AbstractController
 {
@@ -38,7 +41,7 @@ class DefaultController extends AbstractController
         $categories = $categoryRepository
             ->findAll();
 
-        $placesByAvg = $placeRepository->findAvgRatePlaceByCategory($categoryRepository->findOneBy(['name'=>$category])->getId());
+        $placesByAvg = $placeRepository->findAvgRatePlaceByCategory($categoryRepository->findOneBy(['name' => $category])->getId());
         return $this->render('default/index.html.twig', [
             'places' => $placesByAvg,
             'categories' => $categories,
@@ -70,29 +73,40 @@ class DefaultController extends AbstractController
         $params = $request->request->all();
 
         $place = new Place();
-        $place -> setName($params["name"]);
-        $place -> setAddress($params["address"]);
-        $place -> setPhoneNumber($params["phone"]);
-        $place -> setCategory($categoryRepository
-                ->findOneBy(['id' => $params["category"]]));
-        $place -> setDescription($params["description"]);
-        $place -> setLatitude($params["latitude"]);
-        $place -> setLongitude($params["longitude"]);
+        $place->setName($params["name"]);
+        $place->setAddress($params["address"]);
+        $place->setPhoneNumber($params["phone"]);
+        $place->setCategory($categoryRepository
+            ->findOneBy(['id' => $params["category"]]));
+        $place->setDescription($params["description"]);
+        $place->setLatitude($params["latitude"]);
+        $place->setLongitude($params["longitude"]);
+
+        $files = $request->files->all();
+        foreach ($files as $file) {
+            try {
+                $filename = $file->getClientOriginalName();
+                var_dump($filename);
+                $file->move($this->getParameter('kernel.project_dir') . '/public/images', $filename);  //TODO путь может не работать в онлайне
+            } catch (FileException $e) {
+            }
+        }
 
         $em = $this->getDoctrine()->getManager();
         $em->persist($place);
         $em->flush();
 
-        for($i = 0; $i <= 6; $i++) {
+        for ($i = 0; $i <= 6; $i++) {
             $workTime = new WorkTime();
-            $workTime ->setPlace($place);
-            $workTime -> setStartTime(\DateTime::createFromFormat('H:i', $params[$i . "_start_time"]));
-            $workTime -> setEndTime(\DateTime::createFromFormat('H:i', $params[$i . "_end_time"]));
-            $workTime -> setWeekDay($i);
+            $workTime->setPlace($place);
+            $workTime->setStartTime(\DateTime::createFromFormat('H:i', $params[$i . "_start_time"]));
+            $workTime->setEndTime(\DateTime::createFromFormat('H:i', $params[$i . "_end_time"]));
+            $workTime->setWeekDay($i);
             $em = $this->getDoctrine()->getManager();
             $em->persist($workTime);
             $em->flush();
         }
+
 
         return new Response('success');
     }
