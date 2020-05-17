949b41c267de783b41ff1f1606b35c0bacfa9e7b
YambarshevViktor <vi.yambar@mail.ru>
2020-05-17 16:43:52 +0300

    add frontend

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,19 @@
 ymaps.ready(init);
 
 function init(){
+
+    var longitude = "<?php.echo $place.longitude; ?>";
+    var latitude = "<?php.echo $place.latitude; ?>";
+
     var map = new ymaps.Map("map", {
-        center: [56.630674200455, 47.892130372023],
+        center: [latitude, longitude],
         zoom: 17,
         behaviors: ['drag']
     });
 
-    var placemark = new ymaps.Placemark([56.630674200462, 47.892130372023], {
-        hintContent: 'AAAaaa',
-        balloonContent: 'AAAaaa'
+    var placemark = new ymaps.Placemark([latitude, longitude], {
+        /*hintContent: 'AAAaaa',
+        balloonContent: 'AAAaaa'*/
     });
 
     map.geoObjects.add(placemark);
